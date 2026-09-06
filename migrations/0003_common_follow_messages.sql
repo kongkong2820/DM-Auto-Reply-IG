2701d8539bd26c3a30327918e57ba81a5f6930bf
kongkong2820 <kongkong2820@gmail.com>
2026-09-06 12:19:52 +0900

    fix: preserve legacy follower prompt columns

--- migrations/0003_common_follow_messages.sql
+++ migrations/0003_common_follow_messages.sql
@@ -9,6 +9,3 @@ VALUES (
   'FOLLOW_RETRY_MESSAGE',
   '아직 팔로우가 확인되지 않았어요. 계정을 팔로우한 뒤 아래 버튼을 다시 눌러주세요.'
 );
-
-ALTER TABLE reel_dm_config DROP COLUMN follow_prompt_message;
-ALTER TABLE reel_dm_config DROP COLUMN follow_retry_message;
